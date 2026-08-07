44f905879b241431f370ba3adab632f8a45b612d
Shoaib Ajaib <ajxa@pm.me>
2026-08-07 08:00:44 +0100

    Removed the testing files in int/ and some related testing functions. Cleaned up internal data to remove duplicate genes for the same fine label. Removed redundant html files generated during the vignette building stage. Passing the devtool check without any issues, warnings or notes

--- data-raw/marker_panels.R
+++ data-raw/marker_panels.R
@@ -1,5 +1,4 @@
-marker_panel_file <- "../OneDrive - University of Leeds/adhoc/Ensemble_Cell_Typing/data/CellVoteR_marker_panels.xlsx"
-
+marker_panel_file <- "../OneDrive - University of Leeds/analysis/CellVoteR/data/CellVoteR_marker_panels.xlsx"
 
 marker_panels_names <- openxlsx::getSheetNames(marker_panel_file)
 
@@ -10,5 +9,21 @@ marker_panels <- list(
 marker_panels$GBM <-  lapply(marker_panels_names, \(x) openxlsx::read.xlsx(marker_panel_file, sheet = x)) |>
   setNames(marker_panels_names)
 
+remove_dups <- function(panel_df, panel_name){
+
+  duplicated_genes <- which(duplicated(panel_df))
+
+  if(length(duplicated_genes) > 0) {
+
+    cli::cli_alert_warning("Removed {length(duplicated_genes)} duplicated genes from {panel_name}")
+    panel_df <- panel_df[-duplicated_genes, ]
+
+  } else cli::cli_alert_success("No duplicated genes found in {panel_name}")
+
+  return(panel_df)
+
+}
+
+marker_panels$GBM <- purrr::imap(marker_panels$GBM, remove_dups)
 
 usethis::use_data(marker_panels, overwrite = TRUE)
